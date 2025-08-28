1462e34e4ebae23efc9539253b57ef6dadbb92eb
Hafiz <hafizgt0612@gmail.com>
2025-08-28 10:56:09 +0700

    fix bug dari dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -19,19 +19,34 @@ class DashboardController extends Controller
     public function __invoke(Request $request)
     {
         $categories = Category::count();
-        $vehicles = Vehicle::count();
+        // $vehicles = Vehicle::count();
         $suppliers = Supplier::count();
         $products = Product::count();
         $customers = User::count();
-        $transactions = TransactionDetail::sum('quantity');
-        $transactionThisMonth = TransactionDetail::whereMonth('created_at', now()->month)
-            ->whereYear('created_at', now()->year)
-            ->sum('quantity');
+        $inboundGoodsCount = TransactionDetail::whereHas('transaction', function ($query) {
+            $query->where('type', 'in');
+        })->sum('quantity');
+
+        $outboundGoodsCount = TransactionDetail::whereHas('transaction', function ($query) {
+            $query->where('type', 'out');
+        })->sum('quantity');
+
+        // 3. Hitung jumlah barang keluar bulan ini
+        $outboundThisMonthCount = TransactionDetail::whereHas('transaction', function ($query) {
+            $query->where('type', 'out');
+        })
+        ->whereMonth('created_at', now()->month)
+        ->whereYear('created_at', now()->year)
+        ->sum('quantity');
         $productsOutStock = Product::where('quantity', '<=', 10)->paginate(5);
         $orders = Order::where('status', 0)->get();
 
         // Ambil 5 produk terlaris (barang keluar terbanyak)
         $bestProduct = TransactionDetail::with('product')
+            ->whereHas('transaction', function($query) {
+                // Tambahkan filter ini untuk hanya mengambil transaksi 'out'
+                $query->where('type', 'out');
+            })
             ->whereMonth('created_at', now()->month)
             ->whereYear('created_at', now()->year)
             ->selectRaw('product_id, SUM(quantity) as total')
@@ -50,12 +65,12 @@ class DashboardController extends Controller
 
         return view('admin.dashboard', compact(
             'categories',
-            'vehicles',
             'suppliers',
             'products',
             'customers',
-            'transactions',
-            'transactionThisMonth',
+            'inboundGoodsCount',
+            'outboundGoodsCount',
+            'outboundThisMonthCount',
             'productsOutStock',
             'orders',
             'label',
